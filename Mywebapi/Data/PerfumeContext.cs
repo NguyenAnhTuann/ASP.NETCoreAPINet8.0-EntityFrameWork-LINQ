ba3ff2807d1bc0bb076f41926bb786935d0ef530
ngg_anhtuan <ng_anhtuan@outlook.com>
2024-06-10 14:12:40 +0700

    Demo ASP.NETCoreAPINet8.0-EntityFrameWork

---
+++ Mywebapi/Data/PerfumeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Mywebapi.Data
+{
+    public class PerfumeContext : DbContext
+    {
+        public PerfumeContext(DbContextOptions<PerfumeContext> otp): base (otp)
+        {
+
+        }
+
+        #region DbSet
+        public DbSet<Perfume>? SANPHAMs { get; set; }
+        #endregion 
+
+    }
+}
